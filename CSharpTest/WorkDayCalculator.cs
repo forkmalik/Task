13c6aa1301f47792471d8187c3600b8c82d155b6
Vlad Savchenko <polkamarka@gmail.com>
2020-02-18 20:31:32 +0200

    First add

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
